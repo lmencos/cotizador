56db02b24ff00217837c023fee3e8146a39d29f7
lmencos <lmencos@hotmail.com>
2020-08-20 19:17:14 -0500

    Starting Cotizador,+ Header component

--- .history/src/App_20200819203946.js
+++ .history/src/App_20200819203946.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header 
-        titulo='Cotizador de Seguros'
-      />
+
     </div>
   );
 }
