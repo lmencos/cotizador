b8b2e301ee39b4f4c02f6160afd16747cb0e06dd
lmencos <lmencos@hotmail.com>
2020-08-20 20:01:29 -0500

    Emotion + Header Styled OK

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  max-width: 450px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+  border-radius: 6px;
+`
 
 function App() {
   return (
-    <div className="App">
+    <Contenedor className="App">
       <Header 
         titulo='Cotizador de Seguros'
       />
-    </div>
+    <ContenedorFormulario>
+
+    </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
+//Para dar estilo a la etiqueta header:
+const ContenedorHeader = styled.header`
+  background-color: #00A1C4;
+  padding: 3px;
+  font-weight: bold;
+  color: #FFFFFF;
+  `;
+
+const TextoHeader = styled.h2`
+  font-size: 1.8rem;
+  margin: 0;
+  font-family:'Slabo 9px', serif;
+  text-align: center;
+  `;
+ 
 const Header = ({titulo}) => {
   return ( 
-    <header>
-      <h1>{titulo}</h1>
-    </header>
+      <ContenedorHeader>
+        <TextoHeader >{titulo}</TextoHeader>
+      </ContenedorHeader>
    );
 }
  
